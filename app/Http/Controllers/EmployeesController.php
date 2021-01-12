614aad380fa1ab3af0939b05c7ba62f4fff339fd
mulyonoaja <miha.mulyono@gmail.com>
2021-01-12 22:04:15 +0700

    modif controller employees

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class EmployeesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         // cek koneksi ke DB
